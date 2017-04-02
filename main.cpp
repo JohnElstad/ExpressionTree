cd39c05b110aee2b5732782b6435b8bb8f79938d
johnelstad <johnelstad1@gmail.com>
2017-04-01 20:37:15 -0700

    Have the code down

---
+++ main.cpp
@@ -0,0 +1,149 @@
+#include "Node.h"
+#include "Stack.h"
+#include "BinaryNode.h"
+#include <cstdlib>
+#include <cstring>
+
+using namespace std;
+
+bool isOperator(char c);
+int precedence(char c);  
+Stack* shuntingYard(char* input);
+BinaryNode* makeTree(Stack* stack);
+void print (BinaryNode* node, int indent = 0);
+void printPostfix (BinaryNode* node);
+void printPrefix (BinaryNode* node);
+void printInfix(BinaryNode* nodes);;
+
+
+int main(){
+  char input[50];
+  cin.get(input, 50);
+  Stack* stack = shuntingYard(input);
+  BinaryNode* head = makeTree(stack);
+ 
+  
+  cout << "Tree:" << endl;
+  print(head);
+  cout << "\nPostfix:  " << endl;
+  printPostfix(head);
+  cout << endl << "\nPrefix:" << endl;
+  printPrefix(head);
+  cout << endl << "\nInfix:" << endl;
+  printInfix(head);
+  cout << endl;
+}
+
+void print(BinaryNode* node, int indent){ 
+  
+  if (node->getRight())
+    print(node->getRight(),indent+1);
+  for(int j = 0; j <= indent; j++){
+    cout << "   ";
+  }
+  node->printData();
+  cout << endl;
+  if (node->getLeft()) 
+    print(node->getLeft(),indent+1);
+}
+
+void printPostfix (BinaryNode* node){
+  if (node->getType() == 1){
+    printPostfix(node->getRight());
+    printPostfix(node->getLeft());
+  }
+  node->printData();
+}
+void printPrefix (BinaryNode* node){
+  node->printData();
+  if (node->getType() == 1){
+    printPrefix(node->getRight());
+    printPrefix(node->getLeft());
+  }
+}
+void printInfix(BinaryNode* node){
+  if (node->getType() == 1){
+    printInfix(node->getRight());
+    node->printData();
+    printInfix(node->getLeft());
+  }
+  else{
+    node->printData();
+  }
+  
+}
+
+BinaryNode* makeTree(Stack* stack){
+  BinaryNode* binaryNode;
+  if (stack->peek()->getType() == 1){
+    binaryNode = new BinaryNode(stack->pop()->getCharData());
+    binaryNode->setLeft(makeTree(stack));
+    binaryNode->setRight(makeTree(stack));
+  }
+  else{
+    binaryNode = new BinaryNode(stack->pop()->getIntData());
+  
+  }
+  return binaryNode;
+}
+
+
+Stack* shuntingYard(char* input){
+  int i = 0;
+  Stack* operatorStack = new Stack(); 
+  Stack* outputStack = new Stack();
+  while (input[i]){ 
+    if(isdigit(input[i])){
+      outputStack->push(new Node(atoi(&input[i])));
+      while(isdigit(input[i+1])){ 
+	i++;
+      }
+    }
+    else if (isOperator(input[i])){ 
+      while(operatorStack->peek()){ 
+	if (precedence(input[i]) >= precedence(operatorStack->peek()->getCharData())){
+	    break;
+	  }
+	outputStack->push(operatorStack->pop());
+	  }
+      operatorStack->push(new Node(input[i]));
+      }
+    else if (input[i] == '('){ 
+      operatorStack->push(new Node(input[i]));
+    }
+    else if (input[i] == ')'){ 
+      while(operatorStack->peek()->getCharData() != '('){ 
+	outputStack->push(operatorStack->pop());
+      }
+      delete operatorStack->pop();
+    }
+    i++;
+  }
+  while(operatorStack->peek()){ 
+    outputStack->push(operatorStack->pop());
+  }
+  delete operatorStack;
+  return outputStack;
+}
+//checks to see if the char is an operator or not
+bool isOperator(char c){
+  if (c == '-' ||
+      c == '+' ||
+      c == '^' ||
+      c == '*' ||
+      c == '/'){
+    return true;
+  }
+  return false;
+}
+//check the order before it puts the op onto the stack
+int precedence(char c){
+  int precedence[256];
+  precedence['-'] = 1;
+  precedence['+'] = 1;
+  precedence['*'] = 2;
+  precedence['/'] = 2;
+  precedence['^'] = 3;
+  precedence['('] = -1;
+  return precedence[c];
+}
